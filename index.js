d6bae5fca01d9b2afbd81d2cc4f9c2fa2b8f954d
Maxwell Lawrence Jann <codejoy@Maxwells-MBP.lan>
2024-03-24 12:06:37 -0700

    progression

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
 console.log("Max Jann Pro");
+
+const deviceType = () => {
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        console.log('tab');
+        return "tablet";
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        console.log('mobile');
+        return "mobile";
+    }
+    console.log("desktop");
+    return "desktop";
+};
+deviceType();
+
+const helloWorld = () => {
+    if(deviceType()=="desktop"){
+        console.log("I see you're on a desktop");
+        document.getElementById("intro").innerHTML=`
+            <p>
+            <span id="goldfirst">W</span>elcome to Cloudland, I see you're on your computer.
+            </p>
+        `
+        // document.getElementById("flying").innerHTML=`
+        // <h3 class="insane">Fly through my  <a href="fly.html">interactive resume</a> </h3>
+        
+        // `
+    }
+    if(deviceType()=="tablet"){
+        console.log("I see you're on a tablet");
+        document.getElementById("intro").innerHTML=`
+            <p>
+            <span id="goldfirst">W</span>elcome to Cloudland, I see you're on instagram.
+            </p>
+        `
+    }
+    if(deviceType()=="mobile"){
+        console.log("I see you're on your phone");
+        document.getElementById("intro").innerHTML=`
+            <p>
+            <span id="goldfirst">W</span>elcome to Cloudland, I see you're on your "cell" phone.
+            </p>
+        `
+    }
+    
+}
+helloWorld();
+
+const nextTime = () =>{
+    let mytiming = new Date();
+    let correcttime = mytiming.getHours();
+    let pikachu = mytiming.getMinutes();
+    let correctday = mytiming.getDay();
+    let day = correctday;
+    let mandela = false;
+    let weekend = false;
+    if(day>=5){
+        weekend = true;
+    }
+    if(day==0){
+        weekend = true;
+    }
+
+    if(correcttime>12){
+        correcttime = correcttime -12;
+    }
+    if(correcttime == 0){
+        correcttime = 12;
+    }
+    console.log('time');
+    if(!weekend){
+        document.getElementById("next").innerHTML=`
+        <p>
+            Is it ${correcttime} o'clock already?
+        </p>
+        <p>
+            Only ${5-day} more days til the weekend
+        </p>
+        `
+    }
+    else{
+        document.getElementById("next").innerHTML=`
+        <p>
+            Is it ${correcttime} o'clock already?
+        </p>
+        <p>Enjoy your weekend</p>
+    `
+    }
+    
+}
+nextTime();
