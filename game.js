3f088b4424bbff80f59aa3f5135fc0ca128401ff
Maxwell Lawrence Jann <codejoy@Maxwells-MBP.lan>
2024-03-24 21:48:32 -0700

    Ye

---
+++ game.js
@@ -0,0 +1,140 @@
+function ghost(){
+    document.getElementById("game").innerHTML=`
+    <div class="center">
+    <iframe frameborder="0" src="https://itch.io/embed-upload/1234744?color=333333" allowfullscreen="" width="1024" height="788"><a href="https://codejoy-wins.itch.io/i-am-forgotten-ghost">Play I am Forgotten Ghost on itch.io</a></iframe>
+    </div>
+    `
+    document.getElementById("instructions").innerHTML=`
+    <h4>I am Ghost Instructions</h4>
+                <p>
+                You are a ghost and your goal is to get to heaven.  Go up not down.
+    </p>
+    <p>
+Noobs - Click on the game or on fullscreen for your keyboard to affect the game
+</p>
+    <p>
+Game by Max Jann
+</p>
+    <p>
+Songs by Kid Cudi and Grandaddy
+</p>
+    <p>
+arrow keys to move
+</p>
+    <p>
+space to jump
+</p>
+    <p>
+i to reset
+</p>
+    <p>
+shift to walk
+</p>
+    <p>
+V to toggle ghost mode
+    </p>
+    <p>
+Collect the powerups to jump higher and run faster
+    </p>
+    <p>Demo Below</p>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/P5cpbYrbvKE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
+function starsenal(){
+    document.getElementById("game").innerHTML=`
+    <iframe frameborder="0" src="https://itch.io/embed-upload/1135453?color=333333" allowfullscreen="" width="1024" height="788"><a href="https://codejoy-wins.itch.io/starsenal-4-the-hunt-for-dark-kirby">Play Starsenal 4 - The Hunt for Dark Kirby on itch.io</a></iframe>
+    `
+    document.getElementById("instructions").innerHTML=`
+    <h4>Starsenal Instructions</h4>
+                <p>You can jump only 3 times but if you hit a bullet in mid air you can get 3 more jumps</p>
+                <p>qweasd to move and rotate.  Shoot with space bar.  Ride the bullets to the end</p>
+                <p>
+                wasd to move
+                </p>
+                <p>
+                q and e to aim
+                </p>
+                <p>
+                i to spin and u and o to correct spin
+                </p>
+                <p>
+                hold and release b for black hole
+                </p>
+                <p>
+                jump on bullets and hunt down dark kirby
+                </p>
+                <p>
+                hit the black star with a black hole to unlock level 2
+                </p>
+    `
+}
+function mad(){
+    document.getElementById("game").innerHTML=`
+    <iframe frameborder="0" src="https://itch.io/embed-upload/1138684?color=333333" allowfullscreen="" width="1024" height="788"><a href="https://codejoy-wins.itch.io/mad-portfolio">Play Mad_Portfolio on itch.io</a></iframe>
+    `
+    document.getElementById("instructions").innerHTML=`
+    <h4>Mad Portfolio Instructions</h4>
+            <p>
+                s to pull up
+            </p>
+            <p> space to go </p>
+            <p> shift to stop </p>
+            <p>wasd to turn </p>
+                <p>
+                Hold J for warp speed
+                </p>
+    `
+}
+function fps(){
+    document.getElementById("game").innerHTML=`
+    <iframe frameborder="0" src="https://itch.io/embed-upload/1202412?color=333333" allowfullscreen="" width="1024" height="788"><a href="https://codejoy-wins.itch.io/fps9">Play fps9 on itch.io</a></iframe>
+    `
+    document.getElementById("instructions").innerHTML=`
+    <h4>FPS Instructions</h4>
+                <p>
+                Hold Space for jet pack
+                </p>
+                <p>
+                Hold Shift for sprint
+                </p>
+                <p>
+                Tap R to teleport
+                </p>
+                <p>
+                Q and E to change weapons
+                </p>
+    `
+}
+function rga(){
+    document.getElementById("game").innerHTML=`
+    <iframe frameborder="0" src="https://itch.io/embed-upload/1076577?color=333333" allowfullscreen="" width="1024" height="788"><a href="https://codejoy-wins.itch.io/rgia3">Play RGIA3 on itch.io</a></iframe>
+    `
+    document.getElementById("instructions").innerHTML=`
+    <h4>Red Grass Instructions</h4>
+                <p>
+                Hold Shift to Sprint
+                </p>
+                <p>
+                Collect the Gems
+                </p>
+    `
+}
+function starbros(){
+    document.getElementById("game").innerHTML=`
+    <iframe frameborder="0" src="https://itch.io/embed-upload/932686?color=333333" allowfullscreen="" width="1024" height="788"><a href="https://codejoy-wins.itch.io/starbros">Play Starbros on itch.io</a></iframe>
+    `
+    document.getElementById("instructions").innerHTML=`
+    <h4>Starbros Instructions</h4>
+                <p>This is the first game I ever designed and still my best.</p>
+                <p>wasd or arrow keys move.  Jump is w up or space bar.  I recommend using wasd and the space bar.</p>
+                <p>I coded it so that you can only jump once in mid air.  You cannot jump again until you hit the "Ground".  I made every object in the game count as the "Ground" so you can spam the space bar in mid air and if you hit something you can double jump.</p>
+                <p>If you can't figure it out there is a youtube demo of it below.</p>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/yxxDWVSn93c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
