393af4859f43f3eacde103fa952953aeef2e4d8a
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-03-25 01:12:48 -0700

    codejoy

---
+++ codemeta.js
@@ -0,0 +1,7 @@
+// script.js
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     document.querySelectorAll('pre code').forEach((el) => {
+//       hljs.highlightElement(el);
+//     });
+//   });
+console.log("metascripting");  
