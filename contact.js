6a63bef361a2b7a51cb9636c70f76d0fd5606db9
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-03-25 01:50:57 -0700

    neverending

--- contact.js
+++ contact.js
@@ -1,3 +1,4 @@
 document.querySelector('.card-container').addEventListener('click', function() {
     this.querySelector('.card').classList.toggle('is-flipped');
+    document.getElementById("xp").innerHTML="";
 });
