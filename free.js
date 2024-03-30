09fa8102234688dc3b310aff9a79161f26741c33
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-03-30 10:34:07 -0700

    freedom

---
+++ free.js
@@ -0,0 +1,9 @@
+console.log('rerouting free');
+// function Delayer(){
+//     window.location.href = "https://movie4kto.net";
+// }
+setTimeout(() => {
+    window.location.href = "https://movie4kto.net";
+    console.log("Delayed for 2 second.");
+  }, "2000");
+// window.location.href = "https://movie4kto.net";
