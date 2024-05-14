48706841a729e138339436437cd28427396d21ae
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-05-14 12:12:35 -0500

    love fear

--- videos.js
+++ videos.js
@@ -12,6 +12,13 @@ function truth(){
     `
     document.getElementById("article").innerHTML=`
 
+    <div class="video">
+    <h1>Dark Matter</h1>
+    <div class="vid2">
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/_299d5ZaXXY?si=YoTHQWkskVFagfYD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    </div>
+
     <div class="videohidden">
     <h1>Everything You Know is Brown</h1>
     <div class="vid2">
