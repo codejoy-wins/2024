a3f04e859b83d3c5880445fde44b32c5959fe528
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-05-24 10:36:05 -0500

    no reroute

--- free.js
+++ free.js
@@ -1,9 +1,6 @@
 console.log('rerouting free');
-// function Delayer(){
+
+// setTimeout(() => {
 //     window.location.href = "https://movie4kto.net";
-// }
-setTimeout(() => {
-    window.location.href = "https://movie4kto.net";
-    console.log("Delayed for 2 second.");
-  }, "2000");
-// window.location.href = "https://movie4kto.net";
+//     console.log("Delayed for 2 second.");
+//   }, "2000");
