d3ae1ca573087650526f382b37f681554f7f2daf
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-07-24 04:37:20 -0500

    instatacular

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ const helloWorld = () => {
             </p>
         `
         document.getElementById("maxster").innerHTML=`
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/U3ZmHenhUdI?si=UD-8sePx4RAQtFdB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/U3ZmHenhUdI?si=UD-8sePx4RAQtFdB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         `
     }
     if(deviceType()=="mobile"){
