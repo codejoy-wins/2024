7f84112af32aee2c058b091b25d18cd3ad09b7bb
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2025-01-10 12:41:56 -0600

    love

--- videos.js
+++ videos.js
@@ -560,6 +560,13 @@ function chess(){
     document.getElementById("article").innerHTML=`
 
     <div class="video2">
+    <h1>Gladiator Chess</h1>
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/ItIQOybH5RA?si=EXCXG7PKYBI6L-FZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    
+
+    <div class="video2">
     <h1>Pro Chess Video</h1>
     <iframe width="100%" height="315" src="https://www.youtube.com/embed/BZZlAmanEE8?si=tusfShGAGG0dgL46" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
