cda263f6c6a563dcdc18b9f31837d8be6c44d4f8
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2025-03-19 08:28:03 -0500

    yeah

--- videos.js
+++ videos.js
@@ -21,6 +21,15 @@ function truth(){
     </div>
 
     <div class="video">
+    <h1>Permanary</h1>
+    <div class="vid2">
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/RqDr8y6IPxk?si=yI3M7M7L_fi9MRf6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    </div>
+
+    
+
+    <div class="video">
     <h1>When Lies become Truth</h1>
     <div class="vid2">
     <iframe width="100%" height="315" src="https://www.youtube.com/embed/GYeZDYJsoHc?si=04nWdPrhS4QOVH1U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
