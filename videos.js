4f92e8c451c7509fa76fb67f35cca7c8500147fe
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2025-07-26 20:14:12 -0500

    in jesus

--- videos.js
+++ videos.js
@@ -319,7 +319,7 @@ function coding(){
         Shock Rifle Combo
     </h1>
     <div class="vid2">
-    <iframe width="100%" height="315" src="https://www.youtube.com/embed/ugfcQ3GaoOE?si=NV_BaS6TRI4gcd0L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/zvxJQ_KrGFI?si=jkULoQzCTajtRE3f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 </div>
 
